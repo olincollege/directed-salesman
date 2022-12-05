656b32b84d02d5c5b6d9fc8f15fd0e3731371737
Han Vakil <hanvakil@Hans-MacBook-Pro.local>
2022-12-05 11:33:27 -0500

    circle working, rect wip

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ Utility functions
 import itertools
 import networkx as nx
 import random
+import math
 
 
 def random_graph(n: int, is_directed: bool = False) -> nx.Graph:
@@ -36,6 +37,57 @@ def random_graph(n: int, is_directed: bool = False) -> nx.Graph:
                                       for i, j in itertools.combinations(range(n), 2))
     return graph
 
+def circle_graph(n: int, is_directed: bool = False) -> nx.Graph:
+    """
+    Generates a complete graph of the outline of a circle
+
+    :param n: an int, the number of nodes in the graph
+    :return: a circular Graph with n nodes
+    """
+    graph = nx.DiGraph()
+    #graph.add_nodes_from(range(n))
+    points = []
+    step_size_rad = 2 * math.pi / n
+    for k in range(n):
+        angle = k * step_size_rad
+        point = (math.sin(angle), math.cos(angle))
+        points.append(point)
+        graph.add_node(k, pos=point)
+
+    # for each pair of nodes, calculate distance and add weighted edge
+    graph.add_weighted_edges_from((i, j, ((points[i][0] - points[j][0]) ** 2
+                                            + (points[i][1] - points[j][1]) ** 2) ** 0.5)
+                                    for i, j in itertools.combinations(range(n), 2))
+    graph.add_weighted_edges_from((j, i, ((points[i][0] - points[j][0]) ** 2
+                                            + (points[i][1] - points[j][1]) ** 2) ** 0.5)
+                                    for i, j in itertools.combinations(range(n), 2))
+    return graph
+
+def rectangle_graph(n: int, m: int, is_directed: bool = False) -> nx.Graph:
+    """
+    Generates a completed grid graph with a point on every vertex of the grid
+
+    :param n: an int, the width of the grid in the graph
+    :param m: an int, the height of the grid in the graph
+    :return: a graph on a filled in grid of nodes with n * m vertices
+    """
+    graph = nx.DiGraph()
+    #graph.add_nodes_from(range(n))
+    points = []
+    for k in range(n):
+        for l in range(m):
+            point = (k/n, l/m)
+            points.append(point)
+            graph.add_node(k, pos=point)
+
+    # for each pair of nodes, calculate distance and add weighted edge
+    graph.add_weighted_edges_from((i, j, ((points[i][0] - points[j][0]) ** 2
+                                            + (points[i][1] - points[j][1]) ** 2) ** 0.5)
+                                    for i, j in itertools.combinations(range(n), 2))
+    graph.add_weighted_edges_from((j, i, ((points[i][0] - points[j][0]) ** 2
+                                            + (points[i][1] - points[j][1]) ** 2) ** 0.5)
+                                    for i, j in itertools.combinations(range(n), 2))
+    return graph
 
 def length_of_path(graph, path):
     """
