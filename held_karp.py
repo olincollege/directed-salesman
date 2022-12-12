ba3bd9d6bc601645d0f6737b910dad9b8e5b2f6f
benmor20 <benmor2020@gmail.com>
2022-12-12 12:21:41 -0500

    Held-Karp complexity

--- held_karp.py
+++ held_karp.py
@@ -15,12 +15,14 @@ def held_karp(graph: nx.Graph) -> Tuple[float, List[int]]:
     :param graph: the Graph to find a path through, with nodes labeled 0, 1, ...
     :return: a tuple, the length of the shortest complete cycle, the order of the nodes
     """
+    # Setup
     cache = {}
     num_nodes = len(graph)
     make_bitset = lambda s: BitSet(num_nodes, s)
     for k in range(1, num_nodes):
         cache[(make_bitset(1 << k), k)] = graph[0][k]['weight'], [0, k]
 
+    # Part 1: populate cache
     for subset_size in range(2, num_nodes):
         for subset_lst in itertools.combinations(range(1, num_nodes), r=subset_size):
             subset = make_bitset(set(subset_lst))
@@ -35,6 +37,7 @@ def held_karp(graph: nx.Graph) -> Tuple[float, List[int]]:
                         shortest = act_dist, path + [k]
                 cache[(subset, k)] = shortest
 
+    # Part 2: Find the shortest path from cache
     shortest = -1, []
     all_nodes = make_bitset(2 ** num_nodes - 2)
     for k in range(1, num_nodes):
