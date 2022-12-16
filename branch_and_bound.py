858a7e721553720bb224fa4ef8171416bcaa0bd5
Han Vakil <hanumanbv@gmail.com>
2022-12-15 23:35:19 -0500

    additional documentation

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -14,6 +14,17 @@ def branch_and_bound(graph: nx.Graph):
     returns:
     :cost: a float representing the "cost" of this path through the graph. The sum of all edges taken.
     :path: a list of ints representing the ordered path the solution takes through the graph.
+
+    Time complexity average O(n^3):
+     - n from expanding nodes on the order of O(n) (maybe 2n, maybe 3n but still order n)
+     - n^2 from reducing the adjacency matrix (of every node so we multiply). Adj_mat is of size n^2
+     so reducing is of order O(n^2)
+    Multiply to get O(n^3) for cases where the number of path nodes expanded is proportional to the number 
+    of nodes in the graph.
+
+    Time complexity worst O(n!):
+     - if we expand all or most path nodes, we expand path nodes on the order of n!
+     - n^2 to reduce adjacency matrix like before but that's subsumed into the factorial so we can ignore
     """
 
     root = helpers.Root_Node(graph)
