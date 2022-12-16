4faff6306eedf584aea664224238c90ae2bd828d
Han Vakil <hanumanbv@gmail.com>
2022-12-15 20:09:06 -0500

    docstring work

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -3,14 +3,22 @@ import networkx as nx
 import numpy as np
 import branch_helpers as helpers
 
-def branch_and_bound(graph):
+def branch_and_bound(graph: nx.Graph):
     """
-    
+    Takes an input graph, uses a branch and bound algorithm to find the shortest path throgh
+    the graph that touches every vertex once, and returns the cost of that path plus the path 
+    itself. Uses a bounding strategy of only keeping the [list_cap] most promising nodes to consider.
+
+    :param graph: An nx graph to be evaluated
+
+    returns:
+    :cost: a float representing the "cost" of this path through the graph. The sum of all edges taken.
+    :path: a list of ints representing the ordered path the solution takes through the graph.
     """
 
     root = helpers.Root_Node(graph)
     sorted_active_nodes = [root]
-    list_cap = 10
+    list_cap = 20
     while True:
         node = sorted_active_nodes[0]
         if node.remaining_graph_nodes == []:
